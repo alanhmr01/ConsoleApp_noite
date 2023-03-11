0749b45516811805eb587233d6ac9ced3c58676a
alanhmr01 <alan.hmr@gmail.com>
2023-03-10 21:59:13 -0300

    ok 2

--- ConsoleApp_noite/Cliente.cs
+++ ConsoleApp_noite/Cliente.cs
@@ -12,38 +12,24 @@ namespace ConsoleApp_noite
         private int Id;
         private string Nome;
         private string Telefone;
+        private Endereco endereco;
 
-    public Cliente(int id, string nome, string telefone)
+    public Cliente(int id, string nome, string telefone, Endereco _endereco)
         {
             Id = id;
             Nome = nome;
             Telefone = telefone;
+            endereco = _endereco;
         }
 
-    public string getNome()
-        {
-            return Nome; 
-        }
-    public void setNome(string nome)
-        {
-            Nome = nome;
-        }
-    public string getTelefone() 
-        {
-            return Telefone;
-        }
-    public void setTelefone(string telefone)
-        {
-            Telefone = telefone;
-        }
-    public int getId()
-        {
-            return Id;
-        }
-    public void setId(int id)
-        {
-            Id = id;
-        }
+        public void setEndereco(Endereco _endereco){ endereco = _endereco;}
+        public Endereco getEndereco(){return endereco;}
+        public string getNome(){return Nome;}
+        public void setNome(string nome){Nome = nome;}
+        public string getTelefone(){return Telefone;}
+        public void setTelefone(string telefone){Telefone = telefone;}
+        public int getId(){return Id;}
+        public void setId(int id){Id = id;}
     }
 
 }
--- ConsoleApp_noite/Program.cs
+++ ConsoleApp_noite/Program.cs
@@ -11,3 +11,14 @@ p1.setValor(11.85M);
 p1.setDescricao("Cerveja SKOL Litrão");
 
 Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
+
+Endereco endereco = new Endereco("Rua Mario Miziara", "91", "Sobral", "159906304", "Taquaritinga", "SP");
+
+Cliente c1 = new Cliente(1, "Gabriele", "997935682", endereco);
+Cliente c2 = new Cliente(1, "Gabriela", "997935682", endereco);
+Cliente c3 = new Cliente(1, "Ryan", "997935682", endereco);
+Cliente c4 = new Cliente(1, "Fernanda", "997935682", endereco);
+Cliente c5 = new Cliente(1, "Giovanni", "997935682", endereco);
+
+Console.WriteLine(c2.getEndereco().getRua() + "" + c2.getEndereco().getNumero());
+c2.getEndereco().setNumero("135");
